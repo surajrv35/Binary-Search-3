ab5e4f80a8ffdae9338de29a30fc6d953c74966c
Suraj Raghavendra Vadvadgi <45305881+surajrv35@users.noreply.github.com>
2020-03-19 00:22:18 -0500

    k closest elements to x using binary search

---
+++ find-k-closest-elements.java
@@ -0,0 +1,33 @@
+// Time Complexity : O(logN+k)
+// Space Complexity : O(k)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> result = new ArrayList<>();
+        
+        //base case
+        if(arr == null || arr.length == 0)
+            return result;
+        
+        int low=0, high=arr.length-k;
+        //iterate using binary search
+        while(low < high)
+        {
+            int mid = low+(high-low)/2;
+            
+            if(Math.abs(x-arr[mid]) > Math.abs(arr[mid+k]-x))
+                low = mid+1;
+            else
+                high = mid;
+        }
+        
+        for(int i=0;i<k;i++)
+            result.add(arr[low+i]);
+        
+        return result;
+    }
+}
